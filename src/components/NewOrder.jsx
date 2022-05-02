f5559e0ec8807f2526e341f3965a8873f38425c5
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-05-02 16:13:05 +0530

    Update NewOrder.jsx

--- src/components/NewOrder.jsx
+++ src/components/NewOrder.jsx
@@ -1,23 +1,17 @@
 import axios from "axios";
 import { useState } from 'react';
-import { orders } from '../Redux/actions';
 export const NewOrder = () => {
   // Get data of only this user. store it in redux
   // GET /orders?owner_name=john will give you all order of user john
   //  on submit click create a new order, new order has status `Not Accepted`
-  const [orders,setOrders]=useState("")
+  const [orders,setOrders]=useState()
   // axios.get('http://localhost:8080/users', login).then(({ data }) => {
   //   console.log('data:', data);
   // });
- const handleOrders = (e) => {
-   const { className, value } = e.target;
-   setLogin({ ...orders, [className]: value });
- };
-
 
  const handleSubmit = (e) => {
    e.preventDefault();
-   axios.get("http://localhost:8080/orders",login).then(({data})=>{
+   axios.get("http://localhost:8080/users",login).then(({data})=>{
      console.log('data:', data)
    //   alert("user login successfully");
    // axios.get('http://localhost:8080/users').then(({data})=>{
@@ -34,9 +28,7 @@ export const NewOrder = () => {
           type="text"
           name="problem"
           placeholder="Enter problem"
-          onChange={(event) => {
-            handleOrders(event);
-          }}
+          
         />
         {/* This input is readonly, it's coming from redux */}
         <input
@@ -45,18 +37,12 @@ export const NewOrder = () => {
           name="owner_name"
           placeholder="yourname"
           readOnly
-          onChange={(event) => {
-            handleOrders(event);
-          }}
         />
         <input
           className="brand"
           type="text"
           name="brand"
           placeholder="Enter brand name"
-          onChange={(event) => {
-            handleOrders(event);
-          }}
         />
         {/* Create new problem, show it in below form immediately */}
         <button className="submit" onClick={handleSubmit}>
